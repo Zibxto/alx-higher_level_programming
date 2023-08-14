bfb509bbb9f4df8846e0fd0d9407114a8c7e1c93
zibxto <christianaziba@gmail.com>
2023-08-14 20:13:13 +0100

    updated file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     for x in dir(hidden_4):
         if not x.startswith("__"):
-            print("{:s}.format(x)")
+            print("{:s}".format(x))
