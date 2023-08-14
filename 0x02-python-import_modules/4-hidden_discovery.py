92c36b179e3746c471d8fa6e2605841fcb33e9ba
zibxto <christianaziba@gmail.com>
2023-08-14 20:11:20 +0100

    corrected method name

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for x in dir(hidden_4):
-        if not x.startwith("__"):
-            print("{}.format(x)")
+        if not x.startswith("__"):
+            print("{:s}.format(x)")
