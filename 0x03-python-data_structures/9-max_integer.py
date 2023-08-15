f118a52ec05b7d4799c5a0bbb3c6d5c9e00b9476
zibxto <christianaziba@gmail.com>
2023-08-16 00:16:59 +0100

    added file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if my_list == []:
+        return None
+
+    else:
+        i = 0
+        length = len(my_list)
+
+        for x in my_list:
+            while x >= my_list[i]:
+                i += 1
+
+                if i == length - 1:
+                    return x
