ba066ae7deee10716142a68c1d23d61ea278d1e5
zibxto <christianaziba@gmail.com>
2023-08-17 15:55:35 +0100

    updated file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,10 @@ def square_matrix_simple(matrix=[]):
     if matrix is not None:
         result = []
         for x in matrix:
+            row_result = []
             for y in x:
-                result.append(y ** 2)
-            result_list = list(result)
-        return result_list
+                row_result.append(y ** 2)
+            result.append(row_result)
+        return result
     else:
         return None
