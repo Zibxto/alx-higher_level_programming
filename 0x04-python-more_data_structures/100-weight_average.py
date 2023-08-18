af5819058eae2069199ff90bb1057e691932e3f0
zibxto <christianaziba@gmail.com>
2023-08-18 11:19:12 +0100

    added file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is not None:
+        mul = 1
+        total = 0
+        weight = 0
+        for x in my_list:
+            for y in x:
+                mul *= y
+            total += mul
+            mul = 1
+            weight += y
+        result = total / weight
+        return result
+    else:
+        return 0
