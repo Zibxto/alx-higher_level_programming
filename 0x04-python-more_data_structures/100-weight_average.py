bcc2bf08495d2b6fb019daf85fe214274d2f0620
zibxto <christianaziba@gmail.com>
2023-08-18 11:23:48 +0100

    updated file

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -11,6 +11,6 @@ def weight_average(my_list=[]):
             mul = 1
             weight += y
         result = total / weight
-        return result
+        return result if result > 0 else 0
     else:
         return 0
