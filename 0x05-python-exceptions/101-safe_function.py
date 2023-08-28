9af805b45bbb35656afb63e1a391c71a395c78c2
zibxto <christianaziba@gmail.com>
2023-08-28 22:40:06 +0100

    updated file

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-from sys import stderr
-
-
 def safe_function(fct, *args):
+    import sys
     try:
-        return fct(*args)
-    except (ZeroDivisionError, IndexError, TypeError, ValueError) as ex
-        stderr.write("Exception: {}\n".format(ex)
+        ret = fct(*args)
+        return ret
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
