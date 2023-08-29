ec20a514e6f44baf630a5be2ec3cc9d94f258078
zibxto <christianaziba@gmail.com>
2023-08-30 00:52:20 +0100

    added file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Module 2-square
+Defines a square class
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of square side with a default value of 0
+    """
+    def __init__(self, size=0):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of square side with a default value of 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = int(size)
