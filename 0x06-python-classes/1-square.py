1792ae93a9dd5dc7456b748e96d38e0a28d9236b
zibxto <christianaziba@gmail.com>
2023-08-30 00:10:39 +0100

    included documentaion

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,10 +3,21 @@
 """
 Module 1-square
 Defines a class Square
-
 """
 
 
 class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of square side
+    """
     def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of square side
+        """
         self.__size = size
