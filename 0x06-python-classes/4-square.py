cd3fad043e90999250476af8575f67e473e8754b
zibxto <christianaziba@gmail.com>
2023-08-30 01:40:29 +0100

    added file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""
+Module 3-square
+Defines a square class with private attribute size, validates size
+and calculate the cuurent square area
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of square side with a default value of 0
+    """
+    def __init__(self, size=0):
+        """
+        Initializes square
+
+        Attributes:
+            __size (int): size of square side with a default value of 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """"
+        Getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value, if int and >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates and return square area
+
+        Attributes:
+            __size: size of square side with a default value of 0
+        """
+        return (self.__size ** 2)
