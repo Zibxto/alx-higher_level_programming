2c8dd648d1fff1e540e59a96d1a06b92d2cba45c
zibxto <christianaziba@gmail.com>
2023-08-30 02:10:24 +0100

    added file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import sys
+
+"""
+Module 3-square
+Defines a square class with private attribute size, validates size
+and calculate the cuurent square area
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of square side with a default value of 0
+    """
+    def __init__(self, size=0):
+        """
+        Initializes square
+
+        Attributes:
+            __size (int): size of square side with a default value of 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"
+        Getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value, if int and >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates and return square area
+
+        Attributes:
+            __size: size of square side with a default value of 0
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        Prints the square area in # symbol
+
+        Attributes:
+            __size: size of square side with a default value of 0
+        """
+        if self.__size == 0:
+            sys.stdout.write("\n")
+        else:
+            for x in range(self.__size):
+                sys.stdout.write((self.__size) * "#")
+                print()
