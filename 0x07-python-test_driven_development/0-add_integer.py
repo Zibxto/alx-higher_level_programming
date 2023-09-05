d5a59a2b4bcc63d4b4183b23824cb10d23e55308
zibxto <christianaziba@gmail.com>
2023-09-05 14:01:39 +0100

    fixed: pycodestyle error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ Module 0-add_integer
 The add_integer module has a function that adds 2 integers
 """
 
+
 def add_integer(a, b=98):
     """
     function that adds 2 integers a, b
@@ -17,12 +18,12 @@ def add_integer(a, b=98):
          int: the addition of a and b
 
      Raises:
-         TypeError: If a and be are not ints or floats     
+         TypeError: If a and be are not ints or floats
     """
     if isinstance(a, (int)):
         pass
     elif isinstance(a, float):
-            a = int(a)
+        a = int(a)
     else:
         raise TypeError("a must be an integer")
     if isinstance(b, int):
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Module 2-matrix_divided.py
+Contains a function that divides all elements of a matrix
+"""
+
+def matrix_divided(matrix, div):
+    """
+    
+    """
