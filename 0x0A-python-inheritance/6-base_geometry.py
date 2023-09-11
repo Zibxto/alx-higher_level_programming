e58f2cad0f6cdfa259b8be9ef16d7e29c979e405
zibxto <christianaziba@gmail.com>
2023-09-12 00:01:36 +0100

    added file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Contains a function that raises an exception
+"""
+
+
+class BaseGeometry:
+    """ A class with a method called area"""
+    def area(self):
+        """ Raises an exception"""
+        raise Exception("area() is not implemented")
