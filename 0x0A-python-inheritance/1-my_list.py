dbaedc3aad8a423552f64059adb82fea53ff9ce0
zibxto <christianaziba@gmail.com>
2023-09-11 22:38:32 +0100

    fixes error: correct pycodestyle error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,13 +5,14 @@ Module 1-my_list
 Contains a class that inherits list
 """
 
+
 class MyList(list):
     """
     Inherits from list
 
     methods:
         print_sorted: prints a sorted list
-    """ 
+    """
     def print_sorted(self):
         """ prints a list of sorted ints"""
         self.sorted_list = sorted(self)
