e92097468acb0d2262022a50aa51f2da11ce6969
zibxto <christianaziba@gmail.com>
2023-09-15 17:11:37 +0100

    added file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+"""
+Module 7-add_item.py
+Contains a script that adds all arguments to a python list,
+and then save them to a file
+"""
+
+
+json_file = "add_item.json"
+args = sys.argv
+
+try:
+    my_list = load_from_json_file(json_file)
+except FileNotFoundError:
+    my_list = []
+
+save_to_json_file(my_list + args[1:], json_file)
