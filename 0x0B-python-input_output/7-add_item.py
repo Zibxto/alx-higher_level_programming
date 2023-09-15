2da2cb59038327b932a705df6f5100fb3efc75ca
zibxto <christianaziba@gmail.com>
2023-09-15 17:16:45 +0100

    added file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""
+Module 6-add_item.py
+Contains a script that adds all arguments to a python list,    
+and then save them to a file
+"""
+
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-"""
-Module 7-add_item.py
-Contains a script that adds all arguments to a python list,
-and then save them to a file
-"""
 
 
 json_file = "add_item.json"
