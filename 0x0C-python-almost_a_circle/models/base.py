d96da7fa01c9a1cd5eeb68e4e5319424732d8da4
zibxto <christianaziba@gmail.com>
2023-09-18 22:30:55 +0100

    added file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module base
+Contains a class called Base
+"""
+
+
+class Base:
+    """This class represents a base class"""
+    __nb_object = 0
+    def __init__(self, id=None):
+        """ Class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            __class__.__nb_object += 1
+            self.id = __class__.__nb_object
+
