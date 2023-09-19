16da228f5bb202b868d47f921b4111857dade36d
zibxto <christianaziba@gmail.com>
2023-09-19 02:09:48 +0100

    added file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,14 @@ class Base:
         else:
             __class__.__nb_object += 1
             self.id = __class__.__nb_object
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        import json
+        if list_dictionaries is None or list_dictionaries == {}:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
