995fa1b01fe7a5a1e10416bb0574b57880acb285
zibxto <christianaziba@gmail.com>
2023-10-18 14:37:25 +0100

    add file

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
--- delete database; shouldn't fail if non-existent
-
-DROP DATABASE IF EXISTS hbtn_0c_0;
+ -- a script that deletes the database hbtn_0c_0 in your MySQL server
+ -- sql command to delete a database
+ 
+ DROP DATABASE IF EXISTS hbtn_0c_0;
