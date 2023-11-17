1c958ceef5e4c5a83fe58a37733c9d30073f1bd1
zibxto <christianaziba@gmail.com>
2023-11-17 13:41:38 +0100

    add file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" 
-A script that lists all states from the database hbtn_0e_0_usa
+"""
+A script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa
+where name matches the argument
 """
 
 import MySQLdb
@@ -10,9 +12,11 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
+                (argv[4],))
     result = cur.fetchall()
     for row in result:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     conn.close()
