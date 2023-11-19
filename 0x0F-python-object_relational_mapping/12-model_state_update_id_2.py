481946165a8ade3b6c5fa02ff730bec49b3ffa3b
zibxto <christianaziba@gmail.com>
2023-11-20 00:19:22 +0100

    add file

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_name = session.query(State).filter_by(id=2).first()
+    new_name.name = "New Mexico"
+    session.commit()
