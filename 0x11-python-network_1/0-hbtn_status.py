b6aa4771522dde6164a2934e5ca2efd0bf7ca2e0
zibxto <christianaziba@gmail.com>
2023-12-04 23:34:54 +0100

    move import statement to the top of the script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-if __name__ == "__main__":
-    import urllib.request
+import urllib.request
 
+if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
     print("Body response:")
