3e7b4c48baea8fe8d9b5dbb74f839a158efa7e8b
zibxto <christianaziba@gmail.com>
2023-12-04 19:21:28 +0100

    add file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import urllib.request
+    
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+    print("Body response:")
+    print("     - type:", type(body))
+    print("     - content:", body)
+    print("     - utf8 content:", body.decode('utf-8'))
