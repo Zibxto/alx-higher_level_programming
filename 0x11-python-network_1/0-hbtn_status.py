5c6e653b37f857172c5fa4687c6463c088d9c7e5
zibxto <christianaziba@gmail.com>
2023-12-04 19:26:51 +0100

    fix pycodestyle error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 
 if __name__ == "__main__":
     import urllib.request
-    
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
     print("Body response:")
