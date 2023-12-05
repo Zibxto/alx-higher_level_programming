b8a39a95bc84740c29327d770d29471acb953e61
zibxto <christianaziba@gmail.com>
2023-12-05 01:22:42 +0100

    update file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,13 @@ a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
 
-import urllib.request
 from sys import argv
+import urllib.request
+
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(argv[1]) as res:
-            body = res.read()
-            print(body.decode('utf-8'))
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
