e31c19fc21d707bda8d04c88100be978088fd058
zibxto <christianaziba@gmail.com>
2024-01-03 22:02:13 +0100

    add comment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+/**
+ * A script that reads and prints the content of a file
+ */
+
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (err, data) => {
