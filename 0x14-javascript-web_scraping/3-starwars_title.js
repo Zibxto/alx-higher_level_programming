e1ab6f2e745f2f0964a7009a84f6116aaacf2135
zibxto <christianaziba@gmail.com>
2024-01-03 23:11:36 +0100

    add file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  const parsedBody = JSON.parse(body);
+  console.log(parsedBody.title);
+});
