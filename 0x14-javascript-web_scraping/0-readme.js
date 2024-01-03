72d83e42bc000e727bcd4fb6a6c154681411f079
zibxto <christianaziba@gmail.com>
2024-01-03 21:55:43 +0100

    add filePath var

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const filePath = process.argv[2]
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     throw err;
   }
