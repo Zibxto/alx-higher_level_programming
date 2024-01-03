c3d7cd1354edbf3e370c7231edb3ff927270c73b
zibxto <christianaziba@gmail.com>
2024-01-03 19:44:43 +0100

    add file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-const filename = process.argv[2];
 const fs = require('fs');
+const process =  require('process');
+
+const filename = process.argv[2];
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
