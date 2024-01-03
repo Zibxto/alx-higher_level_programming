a36b6a6e36318c218259474cb895f20ec4369758
zibxto <christianaziba@gmail.com>
2024-01-03 20:57:26 +0100

    update file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], function (error) {
-  if (error) console.log(error);
+fs.writeFile(process.argv[2], process.argv[3], function (err) {
+  if (err) console.log(err);
 });
