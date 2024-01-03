e78df6dba23a3c118416af1d738a51219d23776d
zibxto <christianaziba@gmail.com>
2024-01-03 19:36:05 +0100

    update file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const process = require('process');
+let filename = process.argv[2];
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
+fs.readFile(filename, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
