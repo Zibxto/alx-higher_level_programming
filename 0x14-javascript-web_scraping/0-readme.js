00b63c08e50820efb8c4dad476643196a7452101
zibxto <christianaziba@gmail.com>
2024-01-04 13:06:57 -0800

    converted file to unix format

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-// reads a file. The first argument is the file path
-// The content of the file must be read in utf-8
 
 const fs = require('fs');
-
-// file_path
-const args = process.argv.slice(2);
-const filePath = args[0];
-
+const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    console.log(data);
   }
-
-  console.log(data.trim());
 });
+
