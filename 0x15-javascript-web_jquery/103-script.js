9c0c4b9b804dccbc9ce1c81e3763563d550cab66
zibxto <christianaziba@gmail.com>
2024-01-12 21:37:55 +0100

    implement task

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('#btn_translate, #language_code').on('click keypress', (e) => {
+    if ((e.type === 'keypress' && e.which === 13) || e.type === 'click') {
+      const LANG = $('#language_code').val();
+      const URL = `https://hellosalut.stefanbohacek.dev/?lang=${LANG}`;
+      $.get(URL, (data, status) => {
+        $('#hello').text(data.hello);
+        console.log(status);
+      });
+    }
+  });
+});
